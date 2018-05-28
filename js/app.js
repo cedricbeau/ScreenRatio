c36f7fe9154ae766dfdaf33c4e2876d32a9a62ac
cedricbeau <cedric7693@hotmail.fr>
2018-05-28 15:57:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,9 @@
 'use strict';
 
-toggleConsigne()
 checkRatio()
 calcRatio()
 closeAlert()
 clearInput()
-
-
-function toggleConsigne(){
-
-  let btn = document.querySelector('.btn-consignes')
-  
-  function toggleClass(el1, el2, el3) {
-    el1.classList.toggle('is-down')
-    el2.classList.toggle('is-down')
-    el3.classList.toggle('is-down')
-  }
-
-  function toggleEls(){
-    let consigne = document.querySelector('.consignes')
-    let content = document.querySelector('.content')
-    let btn = document.querySelector('.btn-consignes')
-    toggleClass(consigne, content, btn)
-  }
-  
-  btn.addEventListener('click', toggleEls, false)  
-
-}
 
 function checkRatio() {  
 
